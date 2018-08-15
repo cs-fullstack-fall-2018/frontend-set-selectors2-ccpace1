bd0214b54b65aef9b744e45e38ec0ff49dcea412
Kenn <kenn+git@code-crew.org>
2018-08-15 16:36:55 -0500

    Graded 08-15-2018 16:35:46

--- justforrj.js
+++ justforrj.js
@@ -20,3 +20,19 @@ var getpTags = document.getElementsByTagName("p");
 for (y=0;y<getpTags.length;y++){
     getpTags[length].style.color = "yellow";
 }
+
+/*
+
+Put your js script at the bottom of your HTML page to fix some issues.
+
+Question 1:
+	a) Correct
+Questoin 2: 
+	a) Correct
+Question 3:
+	a) Correct
+Question 4:
+	a) Almost complete. You're supposed to change the 4th p tag. The first one is changing.
+
+From Kenn
+*/
